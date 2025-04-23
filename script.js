6403afa9aff650af06abaffa259a65c3f9673961
Nightsper <fahimhossain645@gmail.com>
2025-04-24 00:33:41 +0600

    create function to play a one round

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let rock = "rock"
-let paper = "paper"
-let scissors = "scissors"
+let rock = "rock";
+let paper = "paper";
+let scissors = "scissors";
 
 function getComputerChoice(){
   let random = Math.floor(Math.random() * (2 - 0 + 1) + 0)
@@ -17,8 +17,38 @@ function getComputerChoice(){
 
 function getHumanChoice(){
   let answer = prompt("Pick rock, paper or scissors.")
-  return answer
+  return answer;
 }
 
-let humanScore = 0
-let computerScore = 0
+let humanChoice = getHumanChoice();
+let computerChoice = getComputerChoice();
+
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice == rock && computerChoice == scissors) {
+    ++humanScore
+    console.log(`Computer picked Scissors, You picked Rock. You win! Rock beats scissors. Your current score is: ${humanScore} and computer score is: ${computerScore}`)}
+    else if (humanChoice == scissors && computerChoice == rock) {
+      ++computerScore
+      console.log(`Computer picked Rock, You picked Scissors. You lose! Rock beats Scissors. Your current score is: ${humanScore} and computer score is: ${computerScore}`)}
+      else if (humanChoice == rock && computerChoice == paper) {
+        ++computerScore
+        console.log(`Computer picked Paper, You picked Rock. You lose! Paper beats Rock.Your current score is: ${humanScore} and computer score is: ${computerScore}`)}
+        else if (humanChoice == paper && computerChoice == rock) {
+          ++humanScore
+          console.log(`Computer picked Rock, You picked Paper. You win! Paper beats Rock.Your current score is: ${humanScore} and computer score is: ${computerScore}`)}
+          else if (humanChoice == scissors && computerChoice == paper){
+            ++humanScore
+            console.log(`Computer picked Paper, You picked Scissors. You win! Scissors beat Paper. Your current score is: ${humanScore} and computer score is: ${computerScore}`)}
+            else if (humanChoice == paper && computerChoice == scissors){
+              ++computerScore
+              console.log(`Computer picked Scissors, You picked Paper. You lose! Scissors beat Paper. Your current score is: ${humanScore} and computer score is: ${computerScore}`)}
+              else {console.log("It's a tie")}
+}
+
+playRound(humanChoice, computerChoice)
+
+/console.log(playRound(humanChoice, computerChoice))
