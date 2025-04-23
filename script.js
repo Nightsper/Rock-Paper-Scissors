dc37889a179d34b019ed02f72372300ff06bf9fd
Nightsper <fahimhossain645@gmail.com>
2025-04-24 04:45:26 +0600

    update script.js

--- script.js
+++ script.js
@@ -20,33 +20,32 @@ function getHumanChoice(){
   return answer.toLowerCase();
 }
 
-let userChoice = getHumanChoice();
-let computerChoice = getComputerChoice();
+let humanSelection = getHumanChoice();
+let computerSelection = getComputerChoice();
 
-
+function playGame() {
 let humanScore = 0;
 let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
   if (humanChoice == rock && computerChoice == scissors) {
     ++humanScore
-    console.log(`Computer picked Scissors, You picked Rock. You win! Rock beats scissors. Your current score is: ${humanScore} and computer score is: ${computerScore}`)}
+    console.log(`Computer picked Scissors, You picked Rock. Rock beats scissors. You win! Your current score is: ${humanScore} and computer score is: ${computerScore}`)}
     else if (humanChoice == scissors && computerChoice == rock) {
       ++computerScore
-      console.log(`Computer picked Rock, You picked Scissors. You lose! Rock beats Scissors. Your current score is: ${humanScore} and computer score is: ${computerScore}`)}
+      console.log(`Computer picked Rock, You picked Scissors. Rock beats Scissors. You lose! Your current score is: ${humanScore} and computer score is: ${computerScore}`)}
       else if (humanChoice == rock && computerChoice == paper) {
         ++computerScore
-        console.log(`Computer picked Paper, You picked Rock. You lose! Paper beats Rock.Your current score is: ${humanScore} and computer score is: ${computerScore}`)}
+        console.log(`Computer picked Paper, You picked Rock. Paper beats Rock. You lose! Your current score is: ${humanScore} and computer score is: ${computerScore}`)}
         else if (humanChoice == paper && computerChoice == rock) {
           ++humanScore
-          console.log(`Computer picked Rock, You picked Paper. You win! Paper beats Rock.Your current score is: ${humanScore} and computer score is: ${computerScore}`)}
+          console.log(`Computer picked Rock, You picked Paper. Paper beats Rock. You win! Your current score is: ${humanScore} and computer score is: ${computerScore}`)}
           else if (humanChoice == scissors && computerChoice == paper){
             ++humanScore
-            console.log(`Computer picked Paper, You picked Scissors. You win! Scissors beat Paper. Your current score is: ${humanScore} and computer score is: ${computerScore}`)}
+            console.log(`Computer picked Paper, You picked Scissors. Scissors beat Paper. You win! Your current score is: ${humanScore} and computer score is: ${computerScore}`)}
             else if (humanChoice == paper && computerChoice == scissors){
               ++computerScore
-              console.log(`Computer picked Scissors, You picked Paper. You lose! Scissors beat Paper. Your current score is: ${humanScore} and computer score is: ${computerScore}`)}
+              console.log(`Computer picked Scissors, You picked Paper. Scissors beat Paper. You lose! Your current score is: ${humanScore} and computer score is: ${computerScore}`)}
               else {console.log("It's a tie")}
 }
-
-playRound(userChoice, computerChoice)
+}
