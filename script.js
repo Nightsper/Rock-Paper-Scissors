e074c972fb9c17faad8616a104f447a843105631
Nightsper <fahimhossain645@gmail.com>
2025-04-23 23:03:31 +0600

    add function for random computer choice

---
+++ script.js
@@ -0,0 +1,16 @@
+let rock = "rock"
+let paper = "paper"
+let scissors = "scissors"
+
+function getComputerChoice(){
+  let random = Math.floor(Math.random() * (2 - 0 + 1) + 0)
+  if (random == 0){
+    return rock;
+  }
+  else if (random == 1) {
+    return paper;
+  }
+  else if (random == 2) {
+    return scissors;
+  }
+}
