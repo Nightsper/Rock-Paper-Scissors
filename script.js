4aca1c8f1263d15f7a7209649bdbbfce700a8a78
Fahim Hossain <fahimhossain645@gmail.com>
2025-05-27 06:54:20 +0000

    Create a functional UI

--- script.js
+++ script.js
@@ -1,64 +1,170 @@
-let rock = "rock";
-let paper = "paper";
-let scissors = "scissors";
+let humanScore = 0;
+let computerScore = 0;
+
+
+let body = document.querySelector("body")
+
+let playAgain = document.createElement("button")
+playAgain.textContent = "Play Again"
+
+let buttons = document.querySelectorAll(".buttons")
+
+let cScore = document.querySelector("#cScore")
+
+let resultContainer = document.querySelector("#resultContainer")
+
+let botChoice = document.createElement("div")
+
+let result = document.querySelector("#result")
+
+let personChoice = document.createElement("div")
+
+let hScore = document.querySelector("#hScore")
+
+let selection = document.querySelector("#selection")
+
+let reset = document.querySelector("#reset")
+
 
 function getComputerChoice() {
   let random = Math.floor(Math.random() * (2 - 0 + 1) + 0)
   if (random == 0) {
-    return rock;
+    return "";
   }
   else if (random == 1) {
-    return paper;
+    return "";
   }
   else if (random == 2) {
-    return scissors;
+    return "✂️";
   }
 }
-
-let humanScore = 0;
-let computerScore = 0;
-
-let selection = document.querySelector("#selection")
-
-selection.addEventListener("click", (e) => {
-  let target = e.target.textContent.toLowerCase()
- 
-  playRound(target, getComputerChoice)
-  
-})
 
 function playRound(humanChoice, computerChoice) {
-  if (humanChoice == rock && computerChoice == scissors) {
+  
+  resultContainer.insertBefore(botChoice, result)
+  
+  resultContainer.appendChild(personChoice)
+  
+  
+  if (humanChoice == "" && computerChoice == "✂️") {
     ++humanScore
-    console.log(`Computer picked Scissors, You picked Rock. Rock beats scissors. You win! Your current score is: ${humanScore} and computer score is: ${computerScore}`)
+    botChoice.textContent = `${computerChoice}`
+    personChoice.textContent = `${humanChoice}`
+    result.textContent = "You win!"
   }
-  else if (humanChoice == scissors && computerChoice == rock) {
-    ++computerScore
-    console.log(`Computer picked Rock, You picked Scissors. Rock beats Scissors. You lose! Your current score is: ${humanScore} and computer score is: ${computerScore}`)
-  }
-  else if (humanChoice == rock && computerChoice == paper) {
-    ++computerScore
-    console.log(`Computer picked Paper, You picked Rock. Paper beats Rock. You lose! Your current score is: ${humanScore} and computer score is: ${computerScore}`)
-  }
-  else if (humanChoice == paper && computerChoice == rock) {
+  else if (humanChoice == "" && computerChoice == "") {
     ++humanScore
-    console.log(`Computer picked Rock, You picked Paper. Paper beats Rock. You win! Your current score is: ${humanScore} and computer score is: ${computerScore}`)
+    botChoice.textContent = `${computerChoice}`
+    personChoice.textContent = `${humanChoice}`
+    result.textContent = `You win!`
   }
-  else if (humanChoice == scissors && computerChoice == paper) {
+  else if (humanChoice == "✂️" && computerChoice == "") {
     ++humanScore
-    console.log(`Computer picked Paper, You picked Scissors. Scissors beat Paper. You win! Your current score is: ${humanScore} and computer score is: ${computerScore}`)
+    botChoice.textContent = `${computerChoice}`
+    personChoice.textContent = `${humanChoice}`
+    result.textContent = "You win!"
   }
-  else if (humanChoice == paper && computerChoice == scissors) {
+  else if (humanChoice == "✂️" && computerChoice == "") {
     ++computerScore
-    console.log(`Computer picked Scissors, You picked Paper. Scissors beat Paper. You lose! Your current score is: ${humanScore} and computer score is: ${computerScore}`)
+    botChoice.textContent = `${computerChoice}`
+    personChoice.textContent = `${humanChoice}`
+    result.textContent = "You lose!"
   }
-  else { console.log("It's a tie") }
+  else if (humanChoice == "" && computerChoice == "") {
+    ++computerScore
+    botChoice.textContent = `${computerChoice}`
+    personChoice.textContent = `${humanChoice}`
+    result.textContent = "You lose!"
+  }
+  else if (humanChoice == "" && computerChoice == "✂️") {
+    ++computerScore
+    botChoice.textContent = `${computerChoice}`
+    personChoice.textContent = `${humanChoice}`
+    result.textContent = "You lose!"
+  }
+  else {
+    botChoice.textContent = `${computerChoice}`
+    personChoice.textContent = `${humanChoice}`
+    result.textContent = "It's a tie"
+  }
+  
+  
+  cScore.textContent = `Computer's score: ${computerScore}`
+  hScore.textContent = `Your score: 
+  ${humanScore}`
+  
+  
+  if (humanScore == 5) {
+    
+    result.textContent = "YOU WON THE GAME!"
+    
+    buttons.forEach((button) => button.remove())
+    
+    selection.appendChild(playAgain)
+  }
+  
+  else if (computerScore == 5) {
+    
+    result.textContent = "YOU LOST THE GAME!"
+    
+    buttons.forEach((button) => button.remove())
+    
+    reset.remove()
+    
+    selection.appendChild(playAgain)
+    
+  }
 }
 
-/*if (humanScore > computerScore) {
-  console.log("You won!")
+function startOver() {
+  
+  botChoice.remove()
+  personChoice.remove()
+  
+  humanScore = 0
+  computerScore = 0
+  
+  cScore.textContent = `Computer's score: ${computerScore}`
+  
+  hScore.textContent = `Your score: 
+  ${humanScore}`
+  
+  result.textContent = "Choose your pick"
 }
-else if (!(humanScore > computerScore)){
-  console.log("You lost!")
-}
-else {console.log("The game is a tie.")}*/
+
+
+result.textContent = "Choose your pick"
+
+cScore.textContent = `Computer's score: ${computerScore}`
+
+hScore.textContent = `Your score: 
+  ${humanScore}`
+
+
+selection.addEventListener("click", (e) => {
+  let humanChoice = e.target.textContent
+  
+  let computerChoice = getComputerChoice()
+  
+  if (e.target.tagName !== "BUTTON" || e.target == playAgain) {
+    return
+  }
+  
+  playRound(humanChoice, computerChoice)
+})
+
+reset.addEventListener("click", () => {
+  startOver()
+})
+
+playAgain.addEventListener("click", () => {
+  startOver()
+  
+  playAgain.remove()
+  
+  buttons.forEach((button) =>
+    selection.appendChild(button)
+  )
+  
+  body.appendChild(reset)
+})
